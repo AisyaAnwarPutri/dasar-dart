3a4a96ee571cfc977e043de74f22ef4c20aca9bf
Aisya <aisyanwarputri@gmail.com>
2021-03-09 10:06:23 +0700

    dasar dart

--- dasar_dart/bin/dasar_dart.dart
+++ dasar_dart/bin/dasar_dart.dart
@@ -1,5 +1,18 @@
 import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dasar_dart.calculate()}!');
+  // String input = stdin.readLineSync();
+  // double number = double.tryParse(input);
+  int number = int.tryParse(stdin.readLineSync());
+  String output;
+
+output = (number > 0) ? "positif" : "negatif atau nol";
+
+  // if (number > 0) {
+  //   output = ("positif");
+  // } else {
+  //   output = ("negatif atau nol");
+  // }
+   print(output);
 }
