50746875ff5beb8b03c1e41ff78445858281824d
Aisya <aisyanwarputri@gmail.com>
2021-03-09 10:15:21 +0700

    switch

--- dasar_dart/bin/switch.dart
+++ dasar_dart/bin/switch.dart
@@ -4,14 +4,28 @@ import 'dart:io';
 void main(List<String> arguments) {
   int number = int.tryParse(stdin.readLineSync());
 
-  if (number == 0) {
-    print("noll");
-  } else if (number == 1) {
+  switch (number) {
+    case 0:
+    print("nol");
+    break;
+    case 1:
     print("satu");
-  } else if (number == 2) {
+    break;
+    case 2:
     print("dua");
-  }else {
+    break;
+    default:
     print("bilangan lainnya");
   }
+
+  // if (number == 0) {
+  //   print("noll");
+  // } else if (number == 1) {
+  //   print("satu");
+  // } else if (number == 2) {
+  //   print("dua");
+  // }else {
+  //   print("bilangan lainnya");
+  // }
 
 }
