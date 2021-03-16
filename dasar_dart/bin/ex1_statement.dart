30064ad6132c02c6460aacea6eabc5ca56ddc73a
Aisya <aisyanwarputri@gmail.com>
2021-03-16 19:49:07 +0700

    statement dart

---
+++ dasar_dart/bin/ex1_statement.dart
@@ -0,0 +1,12 @@
+import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  // statement dart
+  print('Pemograman Android');
+  print('Menggunakan Dart dan Flutter');
+  int a, b, c;
+  String strA = "A";
+  double d;
+  int i;
+}
