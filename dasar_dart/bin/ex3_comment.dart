55f91874f5516b22c52c859e3da2913c31675dd6
Aisya <aisyanwarputri@gmail.com>
2021-03-16 19:49:29 +0700

    comment dart

---
+++ dasar_dart/bin/ex3_comment.dart
@@ -0,0 +1,15 @@
+import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
+import 'dart:io';
+
+//contoh komentar
+void main(List<String> args) {
+  //komentar satu baris
+  //ini komentar kedua
+  /*
+  **komentar lebih dari 2 baris
+  **ok
+  */
+  ///Komentar untuk dokumentasi
+  ///
+  ///Komentar ini digunakan untuk mendokumentasi sintaks program
+}
